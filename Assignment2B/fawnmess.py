28ad1c05ca86e646c623b7013c69497fb36e6f10
deyaahhhh <deandrafrederica1594@gmail.com>
2025-05-06 17:23:15 +1000

    added scats number as added id in longform

--- Assignment2B/fawnmess.py
+++ Assignment2B/fawnmess.py
@@ -6,25 +6,6 @@ import keras
 from sklearn.preprocessing import MinMaxScaler
 import math
 
-scats_df = pd.read_excel('Resources\Scats_Data_October_2006.xls', sheet_name='Data', skiprows=1)
-print(scats_df.columns)
-scats_df.columns = scats_df.columns.str.strip()
-
-scats_df['Date'] = pd.to_datetime(scats_df['Date'], dayfirst=True)
-
-intervals = [f'V{i:02}' for i in range(96)] #V00 to V95
-
-scats_longform = scats_df.melt(id_vars='Date', value_vars=intervals, var_name='Interval', value_name='Flow')
-
-scats_longform['IntervalNum'] = scats_longform['Interval'].str.extract(r'V(\d+)').astype(int)
-scats_longform['Time'] = pd.to_timedelta(scats_longform['IntervalNum'] * 15, unit='min')
-
-scats_longform['DateTime'] = scats_longform['Date'] + scats_longform['Time']
-
-scats_longform = scats_longform.sort_values('DateTime')
-
-series = scats_longform[['DateTime', 'Flow']]
-
 def findspeed(flow):
     a = -1.4648375
     b = 93.75
@@ -40,9 +21,36 @@ def findspeed(flow):
 
     return max(speed1, speed2) if speed1 >= 0 and speed2 >= 0 else (speed1 if speed1 >= 0 else speed2)
 
-series['speed'] = np.nan
+scats_df = pd.read_excel('Resources/Scats_Data_October_2006.xls', sheet_name='Data', skiprows=1)
+print(scats_df.columns)
+scats_df.columns = scats_df.columns.str.strip()
 
-for flow in series['flow']:
+scats_df['Date'] = pd.to_datetime(scats_df['Date'], dayfirst=True)
+
+intervals = [f'V{i:02}' for i in range(96)] #V00 to V95
+
+scats_longform = scats_df.melt(id_vars=['Date', 'SCATS Number'], value_vars=intervals, var_name='Interval', value_name='Flow')
+
+scats_longform['IntervalNum'] = scats_longform['Interval'].str.extract(r'V(\d+)').astype(int)
+scats_longform['Time'] = pd.to_timedelta(scats_longform['IntervalNum'] * 15, unit='min')
+
+scats_longform['DateTime'] = scats_longform['Date'] + scats_longform['Time']
+
+scats_longform['Date'] = scats_longform['Date'].dt.normalize()
+scats_longform['Hour'] = scats_longform['DateTime'].dt.hour
+
+scats_longform = scats_longform.sort_values('Hour')
+
+series = scats_longform[['SCATS Number','Date', 'Flow']]
+
+hourly_data = scats_longform.groupby(['SCATS Number', 'Date', 'Hour'])['Flow'].sum().reset_index()
+hourly_data['Speed'] = hourly_data['Flow'].apply(findspeed)
+
+
+
+# series['speed'] = np.nan
+
+# for flow in series['Flow']:
     
 
-print(series)
+print(hourly_data)
