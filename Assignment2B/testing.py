10ac4d8a8dfb308a0514affa45044d1511ad0fc3
deyaahhhh <deandrafrederica1594@gmail.com>
2025-05-06 14:22:55 +1000

    converted the scatssite xls to csv and parsed it

--- Assignment2B/testing.py
+++ Assignment2B/testing.py
@@ -2,9 +2,18 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-scats_data = pd.read_excel('Assignment2B\Resources\Scats_Data_October_2006.xls', sheet_name='Data', skiprows=1)
+scats_data = pd.read_excel('Resources/Scats_Data_October_2006.xls', sheet_name='Data', skiprows=1)
 scats_sites = scats_data[['SCATS Number', 'NB_LATITUDE', 'NB_LONGITUDE']].drop_duplicates()
 
+#usecols range 5 because the valid columns that need to be parsed are the first 5, so it removes the trailing semicolons
+scats_sites_data = pd.read_csv('Resources/SCATSSite.csv', sep=';', usecols=range(5), skiprows=9, on_bad_lines='skip', header=0)
+#strip trailing whitespaces from column names
+scats_sites_data.columns = scats_sites_data.columns.str.strip()
+
+
+
+print(scats_data["SCATS Number"].unique())
+print(scats_sites_data.columns)
 G = nx.Graph()
 
 for idx, row in scats_sites.iterrows():
