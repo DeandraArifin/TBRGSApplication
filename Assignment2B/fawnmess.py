8b3a519785eb8dfe75e2c8954bb56714a7df92e4
LucyKirbStomper <lucycolpav@gmail.com>
2025-05-07 13:21:28 +1000

    deya fix + start of model

--- Assignment2B/fawnmess.py
+++ Assignment2B/fawnmess.py
@@ -33,21 +33,24 @@ def dataframe(scats_df):
 
     intervals = [f'V{i:02}' for i in range(96)] #V00 to V95
 
-    scats_longform = scats_df.melt(id_vars=['Date', 'SCATS Number'], value_vars=intervals, var_name='Interval', value_name='Flow')
+    scats_longform = scats_df.melt(id_vars=['Date', 'NB_LATITUDE', 'NB_LONGITUDE'], value_vars=intervals, var_name='Interval', value_name='Flow')
+    # print(scats_longform)
 
     scats_longform['IntervalNum'] = scats_longform['Interval'].str.extract(r'V(\d+)').astype(int)
     scats_longform['Time'] = pd.to_timedelta(scats_longform['IntervalNum'] * 15, unit='min')
 
-    scats_longform['DateTime'] = scats_longform['Date'] + scats_longform['Time']
+    # scats_longform['DateTime'] = scats_longform['Date'] + scats_longform['Time']
 
     scats_longform['Date'] = scats_longform['Date'].dt.normalize()
-    scats_longform['Hour'] = scats_longform['DateTime'].dt.hour
+    scats_longform['Hour'] = (scats_longform['IntervalNum'] * 15) // 60
+
+    # print(scats_longform['DateTime', 'Hour'])
 
     scats_longform = scats_longform.sort_values('Hour')
 
-    #series = scats_longform[['SCATS Number','Date', 'Flow']]
+    series = scats_longform[['NB_LATITUDE','NB_LONGITUDE','Date', 'Flow']]
 
-    hourly_data = scats_longform.groupby(['SCATS Number', 'Date', 'Hour'])['Flow'].sum().reset_index()
+    hourly_data = scats_longform.groupby(['NB_LATITUDE','NB_LONGITUDE', 'Date', 'Hour'])['Flow'].sum().reset_index()
     hourly_data['Speed'] = hourly_data['Flow'].apply(findspeed)
 
     uniqueSCATS = hourly_data['SCATS Number'].unique()
@@ -83,12 +86,42 @@ def makemap(scats_data):
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.show()
+
+def prepare_all_sites(hourly_data, seq_length=24):
+    all_X, all_y = [], []
+    site_ids = hourly_data['SCATS Number'].unique() #might want to change to long/lat instead of scats number
+    scaler = MinMaxScaler()
+    all_feats = hourly_data[['Flow', 'Speed']].values
+    scaler.fit(all_feats)
+    for id in site_ids:
+        data = hourly_data[hourly_data['SCATS Number'] == id].sort_values(['Date', 'Hour'])
+        features = data[['Flow', 'Speed']].values
+        if len(features) <= seq_length: #skips incomplete sequences in sites
+            continue
+        scaled = scaler.transform(features)
+        for i in range(len(scaled) - seq_length):
+            X_seq = scaled[i:i + seq_length]
+            y_seq = scaled[1 + seq_length]
+            all_X.append(X_seq)
+            all_y.append(y_seq)
+    return np.array(all_X), np.array(all_y), scaler
+
+def build_model(input_shape):
+    model = keras.models.Sequential([keras.layers.LSTM(64, input_shape=input_shape), keras.layers.Dense(32, activation='relu')])
+
+def prep(hourly_data):
+    X, y, scaler = prepare_all_sites(hourly_data)
+    split = int(0.8 * len(X))
+    X_train, X_test = X[:split], X[split:] #either side of split
+    y_train, y_test = y[:split], y[split:]
+    model = build_model((X.shape[1], X.shape[2]))
     
 def main():
     scats_df = pd.read_excel('Resources/Scats_Data_October_2006.xls', sheet_name='Data', skiprows=1)
     hourly_data = dataframe(scats_df)
     makemap(scats_df)
     #print(hourly_data)
+    prep(hourly_data)
 
 #haversine works by hs.haversine(loc1, loc2) where locs are (latitude, longitude)    
 
