59b1818fb6a5bda2af57714430450c3908eea2f0
deyaahhhh <deandrafrederica1594@gmail.com>
2025-05-21 15:01:52 +1000

    fixed 24:00 to 00:00

--- Assignment2B/gui.py
+++ Assignment2B/gui.py
@@ -209,7 +209,7 @@ def main():
     time_opt = tk.StringVar(value='')
     time_options = ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', 
                     '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00',
-                    '23:00', '24:00']
+                    '23:00', '00:00']
     time_option_menu = tk.OptionMenu(left_frame, time_opt, *time_options)
     time_option_menu.pack(anchor="w", pady=5)
     
